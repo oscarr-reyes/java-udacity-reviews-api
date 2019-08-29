2ff41c3bc62257c48625af18fa45b9d2c36a26d3
Oscar Reyes <oscarr.reyes17@gmail.com>
2019-08-29 16:06:51 -0500

    * Product Controller tasked implementations
    * Implemented tasked implementations on the products route

--- src/main/java/com/udacity/course3/reviews/controller/ProductsController.java
+++ src/main/java/com/udacity/course3/reviews/controller/ProductsController.java
@@ -1,11 +1,14 @@
 package com.udacity.course3.reviews.controller;
 
+import com.udacity.course3.reviews.entity.Product;
+import com.udacity.course3.reviews.repository.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring REST controller for working with product entity.
@@ -13,39 +16,49 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
+	@Autowired
+	ProductsRepository productsRepository;
 
-    // TODO: Wire JPA repositories here
+	/**
+	 * Creates a product.
+	 * <p>
+	 * 1. Accept product as argument. Use {@link RequestBody} annotation.
+	 * 2. Save product.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+		Product newProduct = this.productsRepository.save(product);
 
-    /**
-     * Creates a product.
-     *
-     * 1. Accept product as argument. Use {@link RequestBody} annotation.
-     * 2. Save product.
-     */
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct() {
-        throw new HttpServerErrorException(HttpStatus.NOT_IMPLEMENTED);
-    }
+		return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+	}
 
-    /**
-     * Finds a product by id.
-     *
-     * @param id The id of the product.
-     * @return The product if found, or a 404 not found.
-     */
-    @RequestMapping(value = "/{id}")
-    public ResponseEntity<?> findById(@PathVariable("id") Integer id) {
-        throw new HttpServerErrorException(HttpStatus.NOT_IMPLEMENTED);
-    }
+	/**
+	 * Finds a product by id.
+	 *
+	 * @param id The id of the product.
+	 * @return The product if found, or a 404 not found.
+	 */
+	@RequestMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable("id") Integer id) {
+		Optional<Product> record = this.productsRepository.findById(new Long(id));
 
-    /**
-     * Lists all products.
-     *
-     * @return The list of products.
-     */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public List<?> listProducts() {
-        throw new HttpServerErrorException(HttpStatus.NOT_IMPLEMENTED);
-    }
+		if (!record.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			Product product = record.get();
+
+			return new ResponseEntity<>(product, HttpStatus.FOUND);
+		}
+	}
+
+	/**
+	 * Lists all products.
+	 *
+	 * @return The list of products.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public List<Product> listProducts() {
+		return this.productsRepository.findAll();
+	}
 }
